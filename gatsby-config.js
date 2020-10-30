a675a82631680d0436576faf47d3b437dfba17b9
Sitc <aranyi8921@gmail.com>
2020-10-30 23:08:52 +0100

    setup 0.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Attila Aranyi`,
+    siteUrl: 'https://aranyi.dev',
+    description: 'Flex',
+  },
+
   plugins: [],
 }
